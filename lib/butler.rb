c9d037cf87ca41ff7f83484ec61312434c0e69f2
Florian Hübsch <fh@ninjaconcept.com>
2015-03-31 17:05:48 +0200

    Remove initializers, move configuration to Butler::Engine.

--- lib/butler.rb
+++ lib/butler.rb
@@ -19,5 +19,11 @@ module Butler
   class Engine < ::Rails::Engine
     # Load all folders and files in locales.
     config.i18n.load_path += Dir[Butler::Engine.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en]
+    config.i18n.default_locale = :en
+    config.assets.precompile += [
+      "admin.js",
+      "admin.css"
+    ]
   end
 end
