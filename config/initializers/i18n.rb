1bf16bf160a7c35be859d7e20e71ed1f76051ff4
Stefan Botzenhart <sb@ninjaconcept.com>
2015-04-08 16:26:06 +0200

    Create i18n initializer in base application instead of providing own one

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,6 +1,1 @@
-Rails.application.config.i18n.available_locales = [:en, :de]
-Rails.application.config.i18n.default_locale = :en
-
 Rails.application.config.i18n.load_path += Dir[Butler::Engine.root.join('config', 'locales', '**', '*.{rb,yml}')]
-
-Rails.application.config.i18n.fallbacks = { 'en-GB' => 'en', 'de-DE' => 'de' }
---
+++ lib/generators/butler/templates/config/initializers/i18n.rb
@@ -0,0 +1,4 @@
+I18n.available_locales = [:en, :de]
+I18n.default_locale = :en
+
+Rails.application.config.i18n.fallbacks = { 'en-GB' => 'en', 'de-DE' => 'de' }
